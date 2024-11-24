bb5879c4d8eafa77d52f2bd7d31219b77d11e29d
Murin Sergey <murin_sergey@vk.com>
2024-11-24 20:24:40 +0300

    Upload project
    Загрузка первого проекта

---
+++ EducationCSharp/Module_01/Quest_152/Quest_152.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module_01.Quest_152
+{
+    internal class Quest_152
+    {
+        public static void Run()
+        {
+            Console.WriteLine();
+            Console.WriteLine("===Запускается задние 1.5.2===");
+            Console.WriteLine("Как вас зовут?");
+            var UserName = Console.ReadLine();
+            Console.WriteLine($"Приятно познакомится, {UserName}");
+        }
+    }
+}
