6a9262e9266acdc057a2175e92991fcc7071a8e0
Murin Sergey <murin_sergey@vk.com>
2024-11-24 20:47:59 +0300

    Update Program.cs
    Добавил комментарий о том, что происходит.

--- Module_01/Program.cs
+++ Module_01/Program.cs
@@ -4,7 +4,8 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //Тут вызываются оба задания
+            //Console.WriteLine("Hello, World!");
             Quest_152.Quest_152.Run();
             Quest_153.Quest_153.Run();
         }
