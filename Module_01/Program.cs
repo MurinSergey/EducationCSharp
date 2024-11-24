7d56228ce63d8f892446d104aad8da4c3975b463
Murin Sergey <murin_sergey@vk.com>
2024-11-24 20:31:07 +0300

    Reupload
    Перезаливка проекта :) Учусь

---
+++ Module_01/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Module_01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Quest_152.Quest_152.Run();
+            Quest_153.Quest_153.Run();
+        }
+    }
+}
