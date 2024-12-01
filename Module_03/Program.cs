bc5126d1eff10ebbee2e7036db8638238bc0b1d8
Murin Sergey <murin_sergey@vk.com>
2024-12-02 00:12:39 +0300

    Задача 3.3.2 - Анкета с типами данных

--- Module_03/Program.cs
+++ Module_03/Program.cs
@@ -7,7 +7,8 @@
             //Quest_323.Quest_323.Run();
             //Quest_324.Quest_324.Run();
             //Quest_327.Quest_327.Run();
-            Quest_328.Quest_328.Run();
+            //Quest_328.Quest_328.Run();
+            Quest_332.Quest_332.Run();
         }
     }
 }
---
+++ Module_03/Quest_332/Quest_332.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module_03.Quest_332
+{
+    internal class Quest_332
+    {
+        public static void Run()
+        {
+            const string USER_NAME = "Sergey";
+            byte age = 35;
+            bool havePet = false;
+            float shoeSize = 32;
+            Console.WriteLine($"Меня зовут {USER_NAME}");
+            Console.WriteLine($"Мне {age} лет");
+            Console.WriteLine($"Есть ли у вас домошнее животное? {havePet}");
+            Console.WriteLine($"Размер моей обуви {shoeSize}");
+        }
+    }
+}
