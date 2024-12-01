edea39740f4f02b1b36a0fb64d82cc025d15e062
Murin Sergey <murin_sergey@vk.com>
2024-12-02 01:38:21 +0300

    Задание 3.7.4 - приведение перечислений

--- Module_03/Program.cs
+++ Module_03/Program.cs
@@ -5,12 +5,13 @@
 
         static void Main(string[] args)
         {
-            Quest_323.Quest_323.Run();
-            Quest_324.Quest_324.Run();
-            Quest_327.Quest_327.Run();
-            Quest_328.Quest_328.Run();
-            Quest_332.Quest_332.Run();
-            Quest_360.Quest_360.Run();
+            //Quest_323.Quest_323.Run();
+            //Quest_324.Quest_324.Run();
+            //Quest_327.Quest_327.Run();
+            //Quest_328.Quest_328.Run();
+            //Quest_332.Quest_332.Run();
+            //Quest_360.Quest_360.Run();
+            Quest_374.Quest_374.Run();
         }
     }
 }
---
+++ Module_03/Quest_374/Quest_374.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module_03.Quest_374
+{
+    internal class Quest_374
+    {
+        public static void Run()
+        {
+            Console.WriteLine();
+            Console.WriteLine("===Запускается задание 3.7.4===");
+
+            Console.Write(">>>Как вас зовут? ");
+            string name = Console.ReadLine();
+            Console.Write(">>>Сколько вам лет? ");
+            byte age = checked((byte)int.Parse(Console.ReadLine()));
+            Console.WriteLine(">>>Вас зовут {0} и вам {1} лет", name, age);
+            Console.Write(">>>Ваш любимый день недели? ");
+            DaysOfWeek fday = (DaysOfWeek) byte.Parse(Console.ReadLine());
+            Console.WriteLine("Ваш любимый день недели - {0}", fday.ToString());   
+
+        }
+    }
+}
