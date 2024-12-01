3efae92e504e9f9ac3c96ef397f1e0418fdd287e
Murin Sergey <murin_sergey@vk.com>
2024-12-02 00:29:15 +0300

    Добавлен класс дней недели с перечислением Enum

--- Module_03/Program.cs
+++ Module_03/Program.cs
@@ -2,13 +2,14 @@
 {
     internal class Program
     {
+
         static void Main(string[] args)
         {
-            Quest_323.Quest_323.Run();
-            Quest_324.Quest_324.Run();
-            Quest_327.Quest_327.Run();
-            Quest_328.Quest_328.Run();
-            Quest_332.Quest_332.Run();
+            //Quest_323.Quest_323.Run();
+            //Quest_324.Quest_324.Run();
+            //Quest_327.Quest_327.Run();
+            //Quest_328.Quest_328.Run();
+            //Quest_332.Quest_332.Run();
         }
     }
 }
