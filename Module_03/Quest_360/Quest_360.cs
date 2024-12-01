257833e56c0a2d3fcf7a5052d05d1c3fa8e3e343
Murin Sergey <murin_sergey@vk.com>
2024-12-02 01:16:48 +0300

    Задание 3.6.0

---
+++ Module_03/Quest_360/Quest_360.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module_03.Quest_360
+{
+    internal class Quest_360
+    {
+        public static void Run()
+        {
+            Console.WriteLine();
+            Console.WriteLine("===Запускается задание 3.6.0===");
+
+            double result = 5 / 2;
+            Console.WriteLine("5 / 2 = {0}", result);
+
+            result = 5.0 / 2.0;
+            Console.WriteLine("5.0 / 2.0 = {0}", result);
+        }
+    }
+}
