7a087552ef22b2bea29cbbcbb450daaaec3feed9
Murin Sergey <murin_sergey@vk.com>
2024-12-02 01:48:47 +0300

    Выполнил финальную работу модуля

--- Module_03/Program.cs
+++ Module_03/Program.cs
@@ -11,7 +11,8 @@
             //Quest_328.Quest_328.Run();
             //Quest_332.Quest_332.Run();
             //Quest_360.Quest_360.Run();
-            Quest_374.Quest_374.Run();
+            //Quest_374.Quest_374.Run();
+            Quest_Final.Quest_Final.Run();
         }
     }
 }
---
+++ Module_03/Quest_Final/Quest_Final.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module_03.Quest_Final
+{
+    internal class Quest_Final
+    {
+        public static void Run()
+        {
+            Console.WriteLine();
+            Console.WriteLine("===Запускается Финальное задание===");
+
+            Console.Write(">>>Как вас зовут? ");
+            var name = Console.ReadLine();
+
+            Console.Write(">>>Сколько вам лет? ");
+            var age = checked((byte)int.Parse(Console.ReadLine()));
+
+            Console.WriteLine("Вас зовут {0} и вам {1} лет", name, age);
+
+            Console.Write(">>>Когда вы родились? ");
+            var birthday = Console.ReadLine();
+
+            Console.WriteLine($"Вы родились в {birthday}");
+
+            Console.ReadKey();
+        }
+    }
+}
