119b1bc1b4dac476ec408072338eab0a8cc3a9bf
Murin Sergey <murin_sergey@vk.com>
2024-12-06 21:03:12 +0300

    Задание 4.1.16 - выбор цвета консоли

---
+++ Module_04/Quest_4116/Quest_4116.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module_04.Quest_4116
+{
+    internal class Quest_4116
+    {
+        public static void Run()
+        {
+            Console.WriteLine();
+            Console.WriteLine("===Запускается задание 4.1.16===");
+
+            Console.WriteLine("Напишите свой любимый цвет на английском с маленькой буквы");
+
+            var color = Console.ReadLine();
+
+            if (color == "red")
+            {
+                Console.BackgroundColor = ConsoleColor.Red;
+                Console.ForegroundColor = ConsoleColor.Black;
+
+                Console.WriteLine("Your color is red!");
+            }
+
+            else if (color == "green")
+            {
+                Console.BackgroundColor = ConsoleColor.Green;
+                Console.ForegroundColor = ConsoleColor.Black;
+
+                Console.WriteLine("Your color is green!");
+            }
+            else
+            {
+                Console.BackgroundColor = ConsoleColor.Cyan;
+                Console.ForegroundColor = ConsoleColor.Black;
+
+                Console.WriteLine("Your color is cyan!");
+            }
+        }
+    }
+}
