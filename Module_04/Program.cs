02744e4067c612eb7651318fcb3736938bd7cda0
Murin Sergey <murin_sergey@vk.com>
2024-12-06 21:40:34 +0300

    Задание 4.2.11 - цикл do...while

--- Module_04/Program.cs
+++ Module_04/Program.cs
@@ -6,7 +6,8 @@
         {
             //Quest_4110.Quest_4110.Run();
             //Quest_4116.Quest_4116.Run();
-            Quest_4118.Quest_4118.Run();
+            //Quest_4118.Quest_4118.Run();
+            Quest_4211.Quest_4211.Run();
 
         }
     }
---
+++ Module_04/Quest_4211/Quest_4211.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module_04.Quest_4211
+{
+    internal class Quest_4211
+    {
+        public static void Run()
+        {
+            Console.WriteLine();
+            Console.WriteLine("===Запускается задание 4.2.11===");
+
+            Console.WriteLine("Напишите свой любимый цвет на английском с маленькой буквы");
+
+            int k = 0;
+
+            do
+            {
+                switch (Console.ReadLine())
+                {
+                    case "red":
+                        Console.BackgroundColor = ConsoleColor.Red;
+                        Console.ForegroundColor = ConsoleColor.Black;
+
+                        Console.WriteLine("Your color is red!");
+                        break;
+                    case "green":
+                        Console.BackgroundColor = ConsoleColor.Green;
+                        Console.ForegroundColor = ConsoleColor.Black;
+
+                        Console.WriteLine("Your color is green!");
+                        break;
+                    case "cyan":
+                        Console.BackgroundColor = ConsoleColor.Cyan;
+                        Console.ForegroundColor = ConsoleColor.Black;
+
+                        Console.WriteLine("Your color is cyan!");
+                        break;
+                    default:
+                        Console.BackgroundColor = ConsoleColor.Yellow;
+                        Console.ForegroundColor = ConsoleColor.Red;
+
+                        Console.WriteLine("Your color is default!");
+                        break;
+                }
+                Console.WriteLine("Цикл while");
+                Console.WriteLine(k);
+                k++;
+            }while (k < 3);
+
+
+        }
+    }
+}
