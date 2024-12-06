edcc54f7257dec2e9ead2320ca816e50837cb1de
Murin Sergey <murin_sergey@vk.com>
2024-12-06 20:52:33 +0300

    Задание 4.1.10

---
+++ Module_04/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Module_04
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Quest_4110.Quest_4110.Run();
+        }
+    }
+}
---
+++ Module_04/Quest_4110/Quest_4110.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module_04.Quest_4110
+{
+    internal class Quest_4110
+    {
+        public static void Run()
+        {
+            Console.WriteLine();
+            Console.WriteLine("===Запускается задание 4.1.10===");
+
+            var inv = true;
+            var result = !inv;
+            // Решение
+            Console.WriteLine(result);
+        }
+    }
+}
