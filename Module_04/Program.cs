a3ea38ac99baf4a3ad60ae69d39c624a8072664e
Murin Sergey <murin_sergey@vk.com>
2024-12-06 21:13:30 +0300

    Задание 4.1.18 - switch/case

--- Module_04/Program.cs
+++ Module_04/Program.cs
@@ -5,7 +5,9 @@
         static void Main(string[] args)
         {
             //Quest_4110.Quest_4110.Run();
-            Quest_4116.Quest_4116.Run();
+            //Quest_4116.Quest_4116.Run();
+            Quest_4118.Quest_4118.Run();
+
         }
     }
 }
---
+++ Module_04/Quest_4118/Quest_4118.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module_04.Quest_4118
+{
+    internal class Quest_4118
+    {
+        public static void Run()
+        {
+            Console.WriteLine();
+            Console.WriteLine("===Запускается задание 4.1.18===");
+
+            Console.WriteLine("Напишите свой любимый цвет на английском с маленькой буквы");
+
+            switch (Console.ReadLine())
+            {
+                case "red":
+                    Console.BackgroundColor = ConsoleColor.Red;
+                    Console.ForegroundColor = ConsoleColor.Black;
+
+                    Console.WriteLine("Your color is red!");
+                    break;
+                case "green":
+                    Console.BackgroundColor = ConsoleColor.Green;
+                    Console.ForegroundColor = ConsoleColor.Black;
+
+                    Console.WriteLine("Your color is green!");
+                    break;
+                case "cyan":
+                    Console.BackgroundColor = ConsoleColor.Cyan;
+                    Console.ForegroundColor = ConsoleColor.Black;
+
+                    Console.WriteLine("Your color is cyan!");
+                    break;
+                default:
+                    Console.BackgroundColor = ConsoleColor.Yellow;
+                    Console.ForegroundColor = ConsoleColor.Red;
+
+                    Console.WriteLine("Your color is default!");
+                    break;
+            }
+        }
+    }
+}
