27887b120efa36e5369b05161e073776403055aa
Murin Sergey <murin_sergey@vk.com>
2024-12-08 20:17:12 +0300

    Задание 4.3.7

--- Module_04/Program.cs
+++ Module_04/Program.cs
@@ -7,8 +7,8 @@
             //Quest_4110.Quest_4110.Run();
             //Quest_4116.Quest_4116.Run();
             //Quest_4118.Quest_4118.Run();
-            Quest_4211.Quest_4211.Run();
-
+            //Quest_4211.Quest_4211.Run();
+            Quest_437.Quest_437.Run();
         }
     }
 }
---
+++ Module_04/Quest_437/Quest_437.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module_04.Quest_437
+{
+    internal class Quest_437
+    {
+        public static void Run()
+        {
+            Console.WriteLine();
+            Console.WriteLine("===Запускается задание 4.3.7===");
+
+            Console.WriteLine("\nВведите сове имя:");
+            var name = Console.ReadLine();
+            for (int i = name.Length - 1; i >= 0; i--) { 
+                Console.Write(name[i]);
+            }
+        }
+    }
+}
