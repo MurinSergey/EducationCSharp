52d6459066d82c04cfb4dfdbe8720b7547989f62
Murin Sergey <murin_sergey@vk.com>
2024-12-17 00:44:53 +0300

    Задание 5.2.17

--- Module_05/Program.cs
+++ Module_05/Program.cs
@@ -8,7 +8,8 @@
             //Quest_516.Quest_516.Run();
             //Quest_527.Quest_527.Run();
             //Quest_528.Quest_528.Run();
-            Quest_5214.Quest_5214.Run();
+            //Quest_5214.Quest_5214.Run();
+            Quest_5218.Quest_5218.Run();
         }
     }
 }
---
+++ Module_05/Quest_5218/Quest_5218.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace Module_05.Quest_5218
+{
+    internal class Quest_5218
+    {
+        static int[] GetArrayFromConsole(int num = 5) //необязательный параметр
+        {
+            var result = new int[num];
+
+            for (int i = 0; i < result.Length; i++)
+            {
+                Console.WriteLine("Введите элемент массива номер {0}", i + 1);
+                result[i] = int.Parse(Console.ReadLine());
+            }
+
+            return result;
+        }
+
+        static int[] SortArray(int[] array)
+        {
+            //Сортируем массив
+            Array.Sort(array);
+            return array;
+        }
+
+        static void ShowArray(int[] array, bool isSort = false)
+        {
+            var arr = array;
+            if (isSort) {
+                arr = SortArray(arr);
+            }
+            foreach (var item in arr) { Console.WriteLine(item); }
+        }
+
+        public static void Run()
+        {
+            Console.WriteLine();
+            Console.WriteLine("===Запускается задание 5.2.14===");
+
+            var array = GetArrayFromConsole(3);
+            ShowArray(array);
+        }
+    }
+}
