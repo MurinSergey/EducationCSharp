236b90bdb916e50c5f03fae5ad8499eb95dc4138
Murin Sergey <murin_sergey@vk.com>
2024-12-16 23:51:40 +0300

    Задание 5.1.5 вынесено в отдельную папку

--- Module_05/Program.cs
+++ Module_05/Program.cs
@@ -2,52 +2,9 @@
 {
     internal class Program
     {
-        static string ShowColor()
-        {
-            Console.WriteLine("Напишите свой любимый цвет на английском с маленькой буквы");
-            var color = Console.ReadLine();
-            switch (color)
-            {
-                case "red":
-                    Console.BackgroundColor = ConsoleColor.Red;
-                    Console.ForegroundColor = ConsoleColor.Black;
-
-                    Console.WriteLine("Your color is red!");
-                    break;
-                case "green":
-                    Console.BackgroundColor = ConsoleColor.Green;
-                    Console.ForegroundColor = ConsoleColor.Black;
-
-                    Console.WriteLine("Your color is green!");
-                    break;
-                case "cyan":
-                    Console.BackgroundColor = ConsoleColor.Cyan;
-                    Console.ForegroundColor = ConsoleColor.Black;
-
-                    Console.WriteLine("Your color is cyan!");
-                    break;
-                default:
-                    Console.BackgroundColor = ConsoleColor.Yellow;
-                    Console.ForegroundColor = ConsoleColor.Red;
-
-                    Console.WriteLine("Your color is default!");
-                    break;
-            }
-
-            return color;
-        }
-
         static void Main(string[] args)
         {
-            var favcolor = new string[3];
-            for (int i = 0; i < 3; i++) {
-                favcolor[i] = ShowColor();
-            }
-
-            Console.WriteLine("Ваши любимые цвета");
-            foreach (var fav in favcolor) { 
-                Console.WriteLine(fav); 
-            }
+            Quest_515.Quest_515.Run();
         }
     }
 }
---
+++ Module_05/Quest_515/Quest_515.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module_05.Quest_515
+{
+    internal class Quest_515
+    {
+        static string ShowColor()
+        {
+            Console.WriteLine("Напишите свой любимый цвет на английском с маленькой буквы");
+            var color = Console.ReadLine();
+            switch (color)
+            {
+                case "red":
+                    Console.BackgroundColor = ConsoleColor.Red;
+                    Console.ForegroundColor = ConsoleColor.Black;
+
+                    Console.WriteLine("Your color is red!");
+                    break;
+                case "green":
+                    Console.BackgroundColor = ConsoleColor.Green;
+                    Console.ForegroundColor = ConsoleColor.Black;
+
+                    Console.WriteLine("Your color is green!");
+                    break;
+                case "cyan":
+                    Console.BackgroundColor = ConsoleColor.Cyan;
+                    Console.ForegroundColor = ConsoleColor.Black;
+
+                    Console.WriteLine("Your color is cyan!");
+                    break;
+                default:
+                    Console.BackgroundColor = ConsoleColor.Yellow;
+                    Console.ForegroundColor = ConsoleColor.Red;
+
+                    Console.WriteLine("Your color is default!");
+                    break;
+            }
+
+            return color;
+        }
+
+        public static void Run()
+        {
+            Console.WriteLine();
+            Console.WriteLine("===Запускается задание 5.1.5===");
+
+            var favcolor = new string[3];
+            for (int i = 0; i < 3; i++)
+            {
+                favcolor[i] = ShowColor();
+            }
+
+            Console.WriteLine("Ваши любимые цвета");
+            foreach (var fav in favcolor)
+            {
+                Console.WriteLine(fav);
+            }
+        }
+    }
+}
