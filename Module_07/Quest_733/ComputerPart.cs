28a6422e402c2eb1d163bf1932a63e0b7e65d035
Murin Sergey <murin_sergey@vk.com>
2025-01-02 10:08:12 +0900

    Задание 7.3.3 - абстрактный класс и метод

---
+++ Module_07/Quest_733/ComputerPart.cs
@@ -0,0 +1,8 @@
+using System;
+
+namespace Module_07.Quest_733;
+
+public abstract class ComputerPart
+{
+    public abstract void Work();
+}
---
+++ Module_07/Quest_733/GraphicCard.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Module_07.Quest_733;
+
+public class GraphicCard : ComputerPart
+{
+    public override void Work()
+    {
+        throw new NotImplementedException();
+    }
+}
