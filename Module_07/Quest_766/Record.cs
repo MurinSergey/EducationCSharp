6692c15c83d9a05dcca5c70edff9cf0e56289875
Murin Sergey <murin_sergey@vk.com>
2025-01-03 21:42:15 +0900

    Задание 7.6.6

---
+++ Module_07/Quest_766/Record.cs
@@ -0,0 +1,10 @@
+using System;
+
+namespace Module_07.Quest_766;
+
+public class Record<T1, T2>
+{
+    public T1 id = default;
+    public T2 value = default;
+    public DateTime dateTime = default;
+}
