df740c6aff351edc6b189db379256daaaeb343ef
Murin Sergey <murin_sergey@vk.com>
2025-01-09 21:54:57 +0900

    Задание 8.1.5 - Создаем метод AddFolder

--- Module_08/Quest_814/ComputeDisk.cs
+++ Module_08/Quest_814/ComputeDisk.cs
@@ -7,9 +7,14 @@ public class ComputeDisk
     public string Name { get; }
     public long Space { get; }
     public long EmptySpace { get; }
+    public Dictionary<string, Folder> folders { get; } = [];
     public ComputeDisk(string name, long space, long emptySpace){
         Name = name;
         Space = space;
         EmptySpace = emptySpace;
+    }
+
+    public void AddFolder (string name){
+        folders.Add(name, new Folder());
     }
 }
---
+++ Module_08/Quest_814/Folder.cs
@@ -0,0 +1,8 @@
+using System;
+
+namespace Module_08.Quest_814;
+
+public class Folder
+{
+    public List<string> Files { get; set; } = [];
+}
