de91783309c377c712bcdaf07d2aa766fc621f79
Murin Sergey <murin_sergey@vk.com>
2025-01-12 22:00:22 +0900

    DriveInfo - Запрос количества дисков

--- Module_08/Program.cs
+++ Module_08/Program.cs
@@ -4,6 +4,21 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        DriveInfo[] drivers = DriveInfo.GetDrives();
+
+        foreach (DriveInfo driver in drivers)
+        {
+            Console.WriteLine("===================================================");
+            Console.WriteLine($"Название: {driver.Name}");
+            Console.WriteLine($"Тип: {driver.DriveType}");
+            if (driver.IsReady)
+            {
+                Console.WriteLine($"Объем: {driver.TotalSize}");
+                Console.WriteLine($"Свободно: {driver.TotalFreeSpace}");
+                Console.WriteLine($"Метка: {driver.VolumeLabel}");
+            }
+            Console.WriteLine("===================================================");
+        }
+        
     }
 }
