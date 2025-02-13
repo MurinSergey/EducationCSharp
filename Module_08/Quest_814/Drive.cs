ac424057a3b882635ab003619c61eb185d450521
Murin Sergey <murin_sergey@vk.com>
2025-02-13 11:52:33 +0300

    Модуль 08 - немного доработал классы из задания 8.1.4

--- Module_08/Quest_814/Drive.cs
+++ Module_08/Quest_814/Drive.cs
@@ -4,17 +4,65 @@ namespace Module_08.Quest_814;
 
 public class Drive(string name, long space, long emptySpace) //Использовал предложение IDE, которая переделелал конструктор
 {
-    enum DriveType{
+    /// <summary>
+    /// Тип диска или директории
+    /// </summary>
+    internal enum DriveType{
         USB,
         HDD,
         CD
     }
+
+    /// <summary>
+    /// Получение имени диска
+    /// </summary>
     public string Name { get; } = name;
+
+    /// <summary>
+    /// Получение общего объема диска
+    /// </summary>
     public long Space { get; } = space;
+
+    /// <summary>
+    /// Получение свободного пространства
+    /// </summary>
     public long EmptySpace { get; } = emptySpace;
+
+    /// <summary>
+    /// Список директорий на диске
+    /// </summary>
     public Dictionary<string, Folder> folders { get; } = [];
 
+    /// <summary>
+    /// Метод для добавления директрий
+    /// </summary>
+    /// <param name="name"></param>
     public void AddFolder (string name){
         folders.Add(name, new Folder(name));
+    }
+}
+
+/// <summary>
+/// Статичный класс для описания методов перечисления DriveType класса Module_08.Quest_814.Drive
+/// </summary>
+public static class DriveTypeMethod
+{
+    /// <summary>
+    /// Метод возвращает текстовое писание выбранного типа носителя
+    /// </summary>
+    /// <param name="type"></param>
+    /// <returns>
+    /// Описание выбранного типа на русском языке
+    /// </returns>
+    /// <exception cref="NotImplementedException"></exception>
+    internal static string ToRussianString(this Drive.DriveType type)
+    {
+        return type switch
+        {
+            Drive.DriveType.USB => "Тип носителя: Переносной носитель USB",
+            Drive.DriveType.HDD => "Тип носителя: Жесткий диск HDD",
+            Drive.DriveType.CD => "Тип носителя: CD/DVD-диск",
+            _ => throw new NotImplementedException()
+        };
     }
 }
--- Module_08/Quest_814/Folder.cs
+++ Module_08/Quest_814/Folder.cs
@@ -4,15 +4,51 @@ namespace Module_08.Quest_814;
 
 public class Folder
 {
+
+    /// <summary>
+    /// Коллекция хранящая поддиректории
+    /// </summary>
+    public Dictionary<string, Folder> Folders = [];
+
+    /// <summary>
+    /// Коллекция файлов в директории
+    /// </summary>
+    public List<string> Files { get; set; } = [];
+
+    /// <summary>
+    /// Хранит имя текущей директории
+    /// </summary>
     string Name { get; set; }
+
+    /// <summary>
+    /// Конструктор новой директрии
+    /// </summary>
+    /// <param name="name"></param>
     public Folder(string name){
         Name = name;
     }
 
-    public List<string> Files { get; } = [];
+    /// <summary>
+    /// Метод создает новую поддиректорию
+    /// </summary>
+    /// <param name="name"></param>
+    public void AddFolder(string name)
+    {
+        if (!Folders.ContainsKey(name))
+        {
+            Folders.Add(name, new Folder(name));
+        }
+    }
+
+    /// <summary>
+    /// Метод добавляем имя файла в коллекцию файлов
+    /// </summary>
+    /// <param name="name"></param>
     public void AddFile(string name){
         if(!Files.Contains(name)){
             Files.Add(name);
         }
     }
+
+
 }
