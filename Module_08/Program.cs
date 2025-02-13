e47e9c0f9d9af550b93ba8e75fb3953707db5ef3
Murin Sergey <murin_sergey@vk.com>
2025-02-13 12:22:17 +0300

    Задание 8.2.1 - Моя версия метода для подсчета количества папок и файлов

--- Module_08/Program.cs
+++ Module_08/Program.cs
@@ -4,6 +4,18 @@ class Program
 {
     static void Main(string[] args)
     {
+        DriveInfo[] drives = PrintDriveInfo();
+        (string[] Foldes, string[] Files) = PrintAllSubFolderObjects(drives[0].Name);
+    }
+
+    /// <summary>
+    /// Метод распечатывает в консоль информацию о носителях
+    /// </summary>
+    /// <returns>
+    /// Возвращается список всех носителей
+    /// </returns>
+    static DriveInfo[] PrintDriveInfo()
+    {
         DriveInfo[] drivers = DriveInfo.GetDrives();
 
         foreach (DriveInfo driver in drivers)
@@ -17,8 +29,63 @@ class Program
                 Console.WriteLine($"Свободно: {driver.TotalFreeSpace}");
                 Console.WriteLine($"Метка: {driver.VolumeLabel}");
             }
-            Console.WriteLine("===================================================");
         }
-        
+
+        return drivers;
+    }
+
+    /// <summary>
+    /// Метод распечатывает информацию о содержимом переданной директории
+    /// </summary>
+    /// <param name="path"></param>
+    /// <returns>
+    /// Возвращается кортеж, содержащий списки папок и файлов (Foldes, Files)
+    /// </returns>
+    /// <exception cref="DirectoryNotFoundException"></exception>
+    static (string[], string[]) PrintAllSubFolderObjects(string path)
+    {
+        string dirName = path;
+        if (Directory.Exists(dirName))
+        {
+            Console.WriteLine("===================================================");
+            Console.WriteLine($"Содержимое директории: {dirName}");
+            Console.WriteLine("Папки:");
+            string[] folders = Directory.GetDirectories(dirName);
+            foreach (string folder in folders)
+            {
+                Console.WriteLine(folder);
+            }
+
+            Console.WriteLine("Файлы:");
+            string[] files = Directory.GetFiles(dirName);
+            foreach (string file in files)
+            {
+                Console.WriteLine(file);
+            }
+
+            Console.WriteLine($"Общее количество папок и файлов: {CalcCountAllSubFolderObjects(dirName)}");
+
+            return (folders, files);
+        }
+        throw new DirectoryNotFoundException("Директория не существует");
+    }
+
+    /// <summary>
+    /// Метод считает количество папок и файлов в переданной директории
+    /// </summary>
+    /// <param name="path"></param>
+    /// <returns>
+    /// Общее количество папок и файлов
+    /// </returns>
+    /// <exception cref="DirectoryNotFoundException"></exception>
+    static int CalcCountAllSubFolderObjects(string path)
+    {
+        string dirName = path;
+        if ( Directory.Exists(dirName) ){
+            string[] folders = Directory.GetDirectories(dirName);
+            string[] files = Directory.GetFiles(dirName);
+            return folders.Length + files.Length;
+        }
+        throw new DirectoryNotFoundException("Директория не существует");
     }
 }
