36b35ed6567aae011d6be464f63fa1728f444f16
Murin Sergey <murin_sergey@vk.com>
2025-02-17 15:19:35 +0300

    Задание 9.3.2 - первый делегат

--- Module_09/Program.cs
+++ Module_09/Program.cs
@@ -4,8 +4,10 @@ using Module_09.Quest_913;
 using Module_09.Quest_914;
 using Module_09.Quest_922;
 using Module_09.Quest_923;
+using Module_09.Quest_932;
 
 //Quest_913.Run();
 //Quest_914.Run();
 //Quest_922.Run();
-Quest_923.Run();
+//Quest_923.Run();
+Quest_932.Run();
---
+++ Module_09/Quest_932/Quest_932.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module_09.Quest_932;
+
+internal class Quest_932
+{
+    private delegate int Calc(int a, int b);
+
+    /// <summary>
+    /// Метод вычитаем значение b из a
+    /// </summary>
+    /// <param name="a"></param>
+    /// <param name="b"></param>
+    /// <returns>
+    /// Результат вычитания b и a
+    /// </returns>
+    private static int Subtract(int a, int b)
+    {
+        return a - b;
+    }
+
+    internal static void Run()
+    {
+        Calc calc = Subtract;
+        var res = calc.Invoke(10, 5);
+        Console.WriteLine($"Результат: {res}");
+    }
+}
