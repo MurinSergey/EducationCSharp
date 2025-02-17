7dbc4a408f56abb0e3a87da8c7fff38a43785c81
Murin Sergey <murin_sergey@vk.com>
2025-02-17 15:36:28 +0300

    Задание 9.3.4 - мультикаст делегата

--- Module_09/Program.cs
+++ Module_09/Program.cs
@@ -5,9 +5,11 @@ using Module_09.Quest_914;
 using Module_09.Quest_922;
 using Module_09.Quest_923;
 using Module_09.Quest_932;
+using Module_09.Quest_934;
 
 //Quest_913.Run();
 //Quest_914.Run();
 //Quest_922.Run();
 //Quest_923.Run();
-Quest_932.Run();
+//Quest_932.Run();
+Quest_934.Run();
---
+++ Module_09/Quest_934/Quest_934.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module_09.Quest_934;
+
+internal class Quest_934
+{
+
+    private delegate void Calc(int a, int b);
+
+    private static void Add(int a, int b)
+    {
+        Console.WriteLine(a + b);
+    }
+
+    private static void Sub(int a, int b)
+    {
+        Console.WriteLine(a - b);
+    }
+
+    internal static void Run()
+    {
+        Calc calc = Add;
+        calc += Sub;
+        calc.Invoke(10, 5);
+    }
+}
