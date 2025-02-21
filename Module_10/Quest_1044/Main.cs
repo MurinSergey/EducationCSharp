1eebeb6b402d3e16ccd29189d2152874cda33ab3
Murin Sergey <murin_sergey@vk.com>
2025-02-21 17:07:21 +0300

    Задание 10.4.4

---
+++ Module_10/Quest_1044/Main.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module_10.Quest_1044
+{
+    class Main
+    {
+        internal static void Run()
+        {
+
+            IUpdater<User> updaterUser = new UserService();
+            updaterUser.Update(new User());
+
+            /*
+             * Контрвариантность
+             * Класс UserService оперирует универсальным типом User, но его можно положить в переменную более специфичного вида Account
+             */
+            IUpdater<Account> updaterAccount = new UserService();
+            updaterAccount.Update(new Account());
+        }
+    }
+}
---
+++ Module_10/Quest_1044/UserService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module_10.Quest_1044
+{
+    class UserService : IUpdater<User>
+    {
+        public void Update(User entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
